ea9b11c7bb69ab84115bea3c51cb034236e40f5f
ESMAILESMAIL <sokot121@chmail.ir>
2016-04-19 19:07:23 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -6,8 +6,8 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "201842724:AAEtJQrZVqjg_d12tBDriOihtBHIp54uKgs" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
-local You = 108800687  --ID ADMIN ایدی خود را اینجا قرار دهید
+local bot_api_key = "201842724:AAEtJQrZVqjg_d12tBDriOihtBHIp54uKgs" --BOT TOKEN
+local You = 108800687  --ID ADMIN
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
